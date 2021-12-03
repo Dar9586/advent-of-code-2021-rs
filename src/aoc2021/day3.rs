6e0715d6fbf606e046b670047b938c7a5e4cb81f
dar9586 <atoppi2012@gmail.com>
2021-12-03 12:45:59 +0100

    Day 3 2021 completed

--- src/aoc2021/day3.rs
+++ src/aoc2021/day3.rs
@@ -17,20 +17,23 @@ fn day3x1() -> i64 {
     }).iter().fold([0, 0], |acc, el| [2 * acc[0] + i64::from(*el > k / 2), 2 * acc[1] + i64::from(*el < k / 2)]).iter().fold(1i64, |acc, y| acc * y)
 }
 
-fn day3x2() -> i64 {
-    let mut numbers0: Vec<String> = BufReader::new(File::open("./inputs/2021/day3.txt").expect("Missing input file")).lines().map(|x| x.unwrap()).collect();
-    let mut numbers1: Vec<String> = numbers0.clone();
-
-    for index in 0..12 {
-        let one_count0 = numbers0.iter().filter(|element| element.chars().nth(index).unwrap() == '1').count();
-        let one_count1 = numbers1.iter().filter(|element| element.chars().nth(index).unwrap() == '1').count();
-
-        if numbers0.len() != 1 {
-            numbers0 = numbers0.iter().filter(|el| el.chars().nth(index).unwrap() == if one_count0 >= (numbers0.len() - one_count0) { '1' } else { '0' }).map(|el| el.to_string()).collect();
+fn day3x2_sub(most: bool) -> i64 {
+    let mut values: Vec<String> = BufReader::new(File::open("./inputs/2021/day3.txt").expect("Missing input file")).lines().map(|x| x.unwrap()).collect();
+    let mut index = 0;
+    while values.len() != 1 {
+        let one_count = values.iter().filter(|element| element.chars().nth(index).unwrap() == '1').count();
+        let len = values.len();
+        if most {
+            values.retain(|el| el.chars().nth(index).unwrap() == if one_count >= (len - one_count) { '1' } else { '0' });
+        } else {
+            values.retain(|el| el.chars().nth(index).unwrap() == if one_count >= (len - one_count) { '0' } else { '1' });
         }
-        if numbers1.len() != 1 {
-            numbers1 = numbers1.iter().filter(|el| el.chars().nth(index).unwrap() == if one_count1 >= (numbers1.len() - one_count1) { '0' } else { '1' }).map(|el| el.to_string()).collect();
-        }
+        index += 1;
     }
-    i64::from_str_radix(numbers0.get(0).unwrap(), 2).unwrap() * i64::from_str_radix(numbers1.get(0).unwrap(), 2).unwrap()
+    i64::from_str_radix(values.get(0).unwrap(), 2).unwrap()
+}
+
+
+fn day3x2() -> i64 {
+    day3x2_sub(false) * day3x2_sub(true)
 }
