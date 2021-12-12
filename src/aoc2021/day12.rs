1de1afb17a32fd02e08538a7a809aa3684823bd8
dar9586 <atoppi2012@gmail.com>
2021-12-12 13:25:18 +0100

    Added day 12 2021

--- src/aoc2021/day12.rs
+++ src/aoc2021/day12.rs
@@ -7,23 +7,23 @@ type Gift = Graph;
 
 #[derive(Debug, Default, Clone)]
 struct Graph {
-    nodes: HashSet<String>,
     edges: HashMap<String, HashSet<String>>,
 }
 
 impl Graph {
     fn add_edge(&mut self, edge: &(&str, &str)) {
-        if self.nodes.insert(edge.0.to_string()) {
+        if !self.edges.contains_key(edge.0) {
             self.edges.insert(edge.0.to_string(), HashSet::new());
         }
-        if self.nodes.insert(edge.1.to_string()) {
+        if !self.edges.contains_key(edge.1) {
             self.edges.insert(edge.1.to_string(), HashSet::new());
         }
         self.edges.get_mut(edge.1).unwrap().insert(edge.0.to_string());
         self.edges.get_mut(edge.0).unwrap().insert(edge.1.to_string());
     }
     fn get_small_caves(&self) -> HashSet<String> {
-        self.nodes.iter().filter(|x| x.chars().next().unwrap().is_ascii_lowercase()).map(|x| x.to_string()).collect()
+        self.edges.keys().filter(|x| x.chars().next().unwrap().is_ascii_lowercase())
+            .map(|x| x.to_string()).collect()
     }
     fn get_adjacent(&self, str: &str) -> &HashSet<String> {
         self.edges.get(str).unwrap()
@@ -35,44 +35,38 @@ fn is_small_cave(str: &str) -> bool {
     str.chars().next().unwrap().is_ascii_lowercase()
 }
 
-fn traverse(node: &str, graph: &Graph, mut small: HashSet<String>, mut path: Vec<String>, small_twice: bool, paths: &mut HashSet<Vec<String>>) {
+fn traverse(graph: &Graph, node: &str, path: &mut Vec<String>, mut seen_twice: bool) -> usize {
     if node == "end" {
-        paths.insert(path);
-        return;
+        return 1;
+    }
+    if is_small_cave(node) && path.contains(&node.to_string()) {
+        if seen_twice || node == "start" {
+            return 0;
+        }
+        seen_twice = true;
     }
     path.push(node.to_string());
-    if is_small_cave(node) {
-        if small.contains(node) && small_twice && node != "start" {
-            graph.get_adjacent(node).iter().for_each(|k| traverse(&k, graph, small.clone(), path.clone(), false, paths));
-        }
-        if !small.remove(node) {
-            return;
-        }
-    }
-
-    graph.get_adjacent(node).iter().for_each(|k| traverse(&k, graph, small.clone(), path.clone(), small_twice, paths));
+    let a = graph.get_adjacent(node).iter()
+        .map(|k| traverse(graph, &k, path, seen_twice)).sum();
+    path.pop();
+    a
 }
 
 #[aoc_generator(day12)]
 fn read_input(input: &str) -> Gift {
     let mut graph = Graph::default();
-    input.lines().map(|line| line.split_once("-").unwrap()).for_each(|e| graph.add_edge(&e));
+    input.lines().map(|line| line.split_once("-").unwrap())
+        .for_each(|e| graph.add_edge(&e));
     graph
 }
 
 #[aoc(day12, part1)]
 fn day1x1(input: &Gift) -> usize {
-    let small = input.get_small_caves();
-    let mut paths = HashSet::new();
-    traverse("start", input, small, Vec::new(), false, &mut paths);
-    paths.len()
+    traverse(input, "start", &mut Vec::new(), true)
 }
 
 
 #[aoc(day12, part2)]
 fn day1x2(input: &Gift) -> usize {
-    let small = input.get_small_caves();
-    let mut paths = HashSet::new();
-    traverse("start", input, small, Vec::new(), true, &mut paths);
-    paths.len()
+    traverse(input, "start", &mut Vec::new(), false)
 }
